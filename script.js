cfb5ae24164405a17decc08f73803e73b21840a5
Devapriya V S <devaapriyavinod@gmail.com>
2026-07-06 22:17:35 +0530

    Fixed mobile navigation and updated portfolio

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
+const menu = document.getElementById("menu-toggle");
 
-menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+const nav = document.getElementById("nav-links");
+
+menu.addEventListener("click",()=>{
+
+    nav.classList.toggle("active");
+
 });
